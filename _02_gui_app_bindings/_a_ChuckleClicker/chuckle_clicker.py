e9bb86b8ad7aa6d513b78c0904e6ab700c283c4f
league <maddoxseul@sbsd.net>
2026-05-16 14:26:12 -0700

    maddox

--- _02_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
+++ _02_gui_app_bindings/_a_ChuckleClicker/chuckle_clicker.py
@@ -27,7 +27,7 @@ class ChuckleClicker(tk.Tk):
         # TODO: Call the joke button's bind() method to call the on_joke()
         #  method when a mouse button is pressed
         #  example: self.joke_button.bind('<ButtonPress>', self.on_joke)
-        #self.buttonjoke.bind('<Button Press>', self.on_joke())
+        self.buttonjoke.bind('<Button Press>', self.on_joke())
         # TODO: Call the joke button's bind() method to call the on_punchline()
         #  method when a mouse button is pressed
 
